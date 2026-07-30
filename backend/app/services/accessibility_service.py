3089a75f8ccd8256349b2a6ace12a77179655590
Sridhar260802 <sridhar26123@gmail.com>
2026-07-30 16:01:28 +0530

    fix and bugs

--- backend/app/services/accessibility_service.py
+++ backend/app/services/accessibility_service.py
@@ -1,36 +1,87 @@
-import requests
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 
 def accessibility_check(url: str):
-    response = requests.get(url, timeout=10)
-    soup = BeautifulSoup(response.text, "html.parser")
 
-    images = soup.find_all("img")
-    buttons = soup.find_all("button")
-    forms = soup.find_all("form")
+    try:
+        with sync_playwright() as p:
 
-    images_without_alt = sum(
-        1 for img in images if not img.get("alt")
-    )
+            browser = p.chromium.launch(
+                headless=True
+            )
 
-    buttons_without_text = sum(
-        1 for btn in buttons if not btn.text.strip()
-    )
+            page = browser.new_page()
 
-    score = 100
+            page.goto(
+                url,
+                wait_until="domcontentloaded",
+                timeout=60000
+            )
+            page.wait_for_timeout(5000)
+            page.screenshot(
+                path="debug.png",
+                full_page=True
+            )
+            print(page.content()) 
+            page.evaluate("""window.scrollTo(0,document.body.scrollHeight);""")
+            page.wait_for_timeout(3000)
+            html = page.content()
 
-    score -= images_without_alt * 5
-    score -= buttons_without_text * 10
+            browser.close()
 
-    if score < 0:
-        score = 0
 
-    return {
-        "total_images": len(images),
-        "images_without_alt": images_without_alt,
-        "buttons": len(buttons),
-        "buttons_without_text": buttons_without_text,
-        "forms": len(forms),
-        "accessibility_score": score
-    }
+        soup = BeautifulSoup(
+            html,
+            "html.parser"
+        )
+
+
+        images = soup.find_all("img")
+        buttons = soup.find_all("button")
+        forms = soup.find_all("form")
+
+
+        images_without_alt = sum(
+            1 for img in images
+            if not img.get("alt")
+        )
+
+
+        buttons_without_text = sum(
+            1 for btn in buttons
+            if not btn.text.strip()
+        )
+
+
+        score = 100
+
+        score -= images_without_alt * 5
+        score -= buttons_without_text * 10
+
+
+        if score < 0:
+            score = 0
+
+
+        return {
+            "total_images": len(images),
+            "images_without_alt": images_without_alt,
+            "buttons": len(buttons),
+            "buttons_without_text": buttons_without_text,
+            "forms": len(forms),
+            "accessibility_score": score
+        }
+
+
+    except Exception as e:
+
+        return {
+            "total_images": 0,
+            "images_without_alt": 0,
+            "buttons": 0,
+            "buttons_without_text": 0,
+            "forms": 0,
+            "accessibility_score": 0,
+            "error": str(e)
+        }
